63923bec008f6ca0f7901874f20c46517ca987b0
Alex Shadley <shadleyalex@gmail.com>
2019-04-05 18:18:27 -0500

    changes forecast to work with floats

--- src/Forecast.elm
+++ src/Forecast.elm
@@ -1,4 +1,4 @@
-module Forecast exposing (Forecast, empty, extend)
+module Forecast exposing (Forecast, empty, extend, dump)
 
 import Time exposing (Weekday(..))
 import Maybe exposing (withDefault)
@@ -7,19 +7,22 @@ import List exposing (head)
 import Week
 
 type Forecast =
-  Forecast (List (Weekday, Int))
+  Forecast (List (Weekday, Float))
 
 empty : Forecast
 empty = Forecast []
 
-extend : Weekday -> Int -> Forecast -> Forecast
+extend : Weekday -> Float -> Forecast -> Forecast
 extend d p (Forecast fs) =
   Forecast ( [(d, p)] ++ fs )
 
-prob : Forecast -> Weekday -> Int
+prob : Forecast -> Weekday -> Float
 prob (Forecast fs) day =
   fs
     |> List.filter (\(d,_) -> d == day)
     |> List.map (\(_,p) -> p)
     |> head
     |> withDefault 0
+  
+dump : Forecast -> List (Weekday, Float)
+dump (Forecast fc) = fc
